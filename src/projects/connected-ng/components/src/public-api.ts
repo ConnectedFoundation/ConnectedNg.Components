db5ed1fd1c556c7bffbf7e6bc106f431dab4e3d1
Matija Koželj <matija.kozelj@tompit.com>
2026-03-18 09:47:07 +0100

    feat: add ide exports

--- src/projects/connected-ng/components/src/public-api.ts
+++ src/projects/connected-ng/components/src/public-api.ts
@@ -1,4 +1,28 @@
+// IDE Components
+export * from './ide/ide';
+export * from './ide/ide-explorer/ide-explorer';
+export * from './ide/ide-explorer/ide-explorer-template';
+export * from './ide/ide-sidebar/ide-sidebar';
+export * from './ide/ide-sidebar/ide-sidebar-tab.directive';
+export * from './ide/ide-toolbox/ide-toolbox';
+export * from './ide/ide-document-editor/ide-document-editor';
+export * from './ide/ide-property-pane/ide-property-pane';
+export * from './ide/ide-pane-content/ide-pane-content';
+
+// IDE Services
+export * from './ide/services/document-service';
 export * from './ide/services/editor-property-service';
+export * from './ide/services/explorer-item-service';
+export * from './ide/services/ide-editor-service';
+export * from './ide/services/ide-layout-service';
+export * from './ide/services/project-service';
+export * from './ide/services/selection-service';
+export * from './ide/services/toolbox-item-service';
+
+// IDE DTOs
+export * from './ide/services/dtos/editor-item';
+export * from './ide/services/dtos/editor-item-property';
+
 export { ComponentsConfigurationProvider as ConfigurationProvider } from './services/service-configuration-initializer';
 export * from './navigation/stack-page/stack-page';
 export * from './navigation/stack-navigation-shell/stack-navigation-shell';
