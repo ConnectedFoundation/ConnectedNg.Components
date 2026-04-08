b8fcc79f13e240faf4d6c326340420aaaf90df71
Janko Jordan <janko.jordan@tompit.com>
2026-04-08 07:14:25 +0200

    feat: ide design

--- src/projects/connected-ng/components/src/ide/ide.ts
+++ src/projects/connected-ng/components/src/ide/ide.ts
@@ -5,6 +5,7 @@ import { CdkPortalOutlet } from "@angular/cdk/portal";
 import { IdeLayoutService } from './services/ide-layout-service';
 import { IdePaneContent } from './ide-pane-content/ide-pane-content';
 import { IdeDocumentEditor } from "./ide-document-editor/ide-document-editor";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'cf-ide',
@@ -13,7 +14,8 @@ import { IdeDocumentEditor } from "./ide-document-editor/ide-document-editor";
     AngularSplitModule,
     CdkPortalOutlet,
     IdePaneContent,
-    IdeDocumentEditor
+    IdeDocumentEditor,
+    MatIcon
   ],
   templateUrl: './ide.html',
   styleUrl: './ide.scss',
