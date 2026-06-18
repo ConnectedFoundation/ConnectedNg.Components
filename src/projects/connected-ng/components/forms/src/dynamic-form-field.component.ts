6b70fbd8c25f6fc5d0e0c8461e82604b552fb48b
HribBlaz <blaz.hribernik@tompit.com>
2026-06-18 21:54:05 +0200

    feat: add custom error support

--- src/projects/connected-ng/components/forms/src/dynamic-form-field.component.ts
+++ src/projects/connected-ng/components/forms/src/dynamic-form-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,6 +11,8 @@ import {
 } from '@ng-matero/extensions/datetimepicker';
 import { FormInputConfig } from './form-input-config';
 import { MatButtonModule } from '@angular/material/button';
+
+const KNOWN_ERROR_KEYS = new Set(['required', 'minlength', 'maxlength', 'min', 'max', 'email']);
 
 @Component({
   selector: 'dynamic-form-field',
@@ -26,7 +28,16 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './dynamic-form-field.component.html',
   styleUrl: './dynamic-form-field.component.scss'
 })
+
 export class DynamicFormFieldComponent {
   fieldConfig = input.required<FormInputConfig>();
   control = input.required<FormControl>();
+
+  customErrors = computed(() => {
+    const errors = this.control().errors;
+    if (!errors) return [];
+    return Object.entries(errors)
+      .filter(([key, value]) => !KNOWN_ERROR_KEYS.has(key) && typeof value === 'string')
+      .map(([, value]) => value as string);
+  });
 }
