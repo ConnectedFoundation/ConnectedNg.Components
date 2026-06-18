50833a61c965d9f33be70cef981e3204b754d36d
HribBlaz <blaz.hribernik@tompit.com>
2026-06-18 13:48:11 +0200

    feat: localize not items found in code-list select-box

--- src/projects/connected-ng/components/code-lists/src/components/code-list-select-box/code-list-select-box.ts
+++ src/projects/connected-ng/components/code-lists/src/components/code-list-select-box/code-list-select-box.ts
@@ -72,6 +72,7 @@ export class CodeListSelectBox<T = any> implements ControlValueAccessor, Validat
   keySelector = input.required<(item: T) => any>();
   displayMemberSelector = input<(item: T) => string>((item: T) => String(item));
   placeholder = input<string>($localize`:@@cn.code-list-select-box.placeholder:Select...`);
+  notFoundText = input<string>($localize`:@@cn.select.no-items-found:No items found`);
   virtualScroll = input<boolean>(false);
   label = input<string>('');
   required = computed(() => this._ngControl()?.control?.hasValidator(Validators.required) ?? false);
